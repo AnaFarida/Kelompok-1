2c08453942e9218c0f799035bd589b0fd77882ad
BagusDuwiPrasetiyo <39429336+bgsadmin@users.noreply.github.com>
2019-09-19 08:58:08 +0700

    Update kelompok1.php

--- kelompok1.php
+++ kelompok1.php
@@ -1,13 +1,14 @@
 <html>
-<head> KELOMPOK 1 </head> <body>
+<head> KELOMPOK 1 </head> 
+<body>
     <?php
     echo "Hello World";
     echo "Nama Ketua : Ana Farida";
-    echo "Nama Anggota : "
-    echo "Bagus Duwi Prasetiyo"
-    echo "Febiola Putri Yunita"
-    echo "Agung Joyo Prassetyo"
-    echo "Ahmad Fadhiel"
+    echo "Nama Anggota : ";
+    echo "Bagus Duwi Prasetiyo";
+    echo "Febiola Putri Yunita";
+    echo "Agung Joyo Prassetyo";
+    echo "Ahmad Fadhiel";
     ?>
 </body>
 </html>
