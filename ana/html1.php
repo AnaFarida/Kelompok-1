eb575ff8a45b68321eba0012ade3338d2f0aa4e6
AnaFarida <55472044+AnaFarida@users.noreply.github.com>
2019-10-01 23:17:04 -0700

    Update html1.php

--- ana/html1.php
+++ ana/html1.php
@@ -7,42 +7,44 @@
 
     <h1 style="color:red">Selamat Belajar HTML</h1>
     <h2 style="color:blue">Ana Farida</h2>
-    <h3> Politekinik Negeri Jember</h3>
+    <h3>Politekinik Negeri Jember</h3>
     <?php 
-    echo "Selamat Pagi Kita akan belajar PHP biar gak dibaperin";
-    $tempat = "di Polije";
-        $Tempat = "di Polije Dua Kampus Bondowoso";
-        $TempaT = "di Polije Tiga";
+    echo "Hallowww Kelas </br></br>";
+    echo "Selamat Pagi!!!! Kita akan belajar PHP biar gak dibaperin";
+    $tempat = " di Politeknik Negeri Jember";
+        $Tempat = " di Politeknik Negeri Jember Dua Kampus Bondowoso";
+        $TempaT = " di Politeknik Negeri Jember";
 
-        echo "<h1 style='color:red'>Selamat Belajar HTML".$tempat."</h1>";
-        ECHO "<h2 style='color:blue'>Selamat Belajar HTML</h2>";
-        EcHo "<h3 style=Selamat Belajar HTML ".$Tempat."</h3>";
+        echo "<h1 style='color:red'> Selamat Belajar HTML" .$tempat."</h1>";
+        ECHO "<h2 style='color:blue'>Selamat Belajar HTML </h2>";
+        EcHo "<h3>Selamat Belajar HTML".$Tempat."</h3>";
         echo $TempaT."</br></br>";
         echo date("Y/m/d")."</br></br>";
 
         //Penjumlahan
         $a = 10; $b = $c = 20; $d = 2;
-        $Jumlah = ($a + $b)/$d;
+        $jumlah = ($a + $b)/$d;
 
-        echo $Jumlah."</br></br>";
+        echo $jumlah."</br></br>";
 
         for ($x=0; $x<= 10; $x++) {
         echo "Tempat Ke - ".$x."<br>";
-        }
+                 }
 
         $t = date("H");
 
+
         if ($t < "20") {
-            echo "Selamat Pagi!";
+        echo "Selamat Pagi!";
         }
 
         //Konstanta
         define("Ana", "Selamat datang di Polije!");
         echo "</br></br>".Ana;
 
-        //fungsi
+        //Fungsi
         function ana(){
-            echo "</br></br>".Ana."Dua";
+            echo "</br></br>".Ana." Dua";
         }
 
         ana();
