dd2bab526ca3335f72c3858191586cfe77a8abbf
ahmadfadhiel <fadhiel.ahmad24@gmail.com>
2019-10-02 10:17:08 +0700

    Create html1.php

---
+++ fadhiel/html1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halo HTML</title>
+    <style>
+    body{
+        background-color: lightblue;
+    }
+    </style>
+</head>
+<body>
+    <h1 style="color: red"> Selamat belajar HTML</h1>
+    <h2 style="color: blue"> Nama Saya : AHMAD FADHIEL</h2>
+    <h3>Politeknik Negeri Jember</h3>
+
+    <?php echo "HELLO PHP"; ?>
+</body>
+</html>
