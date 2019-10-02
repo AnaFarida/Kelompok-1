47cb80bf71d8abcb8aa3918af2a143960df5aeac
AnaFarida <55472044+AnaFarida@users.noreply.github.com>
2019-10-01 21:20:01 -0700

    Update html1.php

--- ana/html1.php
+++ ana/html1.php
@@ -10,30 +10,30 @@
     <h3> Politekinik Negeri Jember</h3>
     <?php 
     echo "Selamat Pagi Kita akan belajar PHP biar gak dibaperin";
-    $Tempat = "di Polije";
-        $Tempat = "di Polije 2 Kampus Bondowoso"
-        $Tempat = "di Polije 3"
+    $tempat = "di Polije";
+        $Tempat = "di Polije Dua Kampus Bondowoso";
+        $TempaT = "di Polije Tiga";
 
-        echo "<h1 style='color:red'>Selamat Belajar HTML".$Tempat."</h1>";
+        echo "<h1 style='color:red'>Selamat Belajar HTML".$tempat."</h1>";
         ECHO "<h2 style='color:blue'>Selamat Belajar HTML</h2>";
         EcHo "<h3 style=Selamat Belajar HTML ".$Tempat."</h3>";
-        echo $Tempat."</br></br>";
+        echo $TempaT."</br></br>";
         echo date("Y/m/d")."</br></br>";
 
         //Penjumlahan
         $a = 10; $b = $c = 20; $d = 2;
         $Jumlah = ($a + $b)/$d;
 
-        echo $Jumlah."</br></br>"
+        echo $Jumlah."</br></br>";
 
-        for ($x = 0; $x <= 10; $x++) {
+        for ($x=0; $x<= 10; $x++) {
         echo "Tempat Ke - ".$x."<br>";
         }
 
         $t = date("H");
 
         if ($t < "20") {
-            echo "Selamat Pagi!"
+            echo "Selamat Pagi!";
         }
 
         //Konstanta
