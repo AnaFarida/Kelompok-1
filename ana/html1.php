c15c326176b83cf86a1ad217294084a63b102e72
AnaFarida <55472044+AnaFarida@users.noreply.github.com>
2019-10-09 09:40:22 +0700

    Update html1.php

--- ana/html1.php
+++ ana/html1.php
@@ -5,7 +5,6 @@
 </head>
 <body>
   
-  \\\\\\\\\\\\\\\\\\\\\\
     <h1 style="color:red">Selamat Belajar HTML</h1>
     <h2 style="color:blue">Ana Farida</h2>
     <h3>Politekinik Negeri Jember</h3>
