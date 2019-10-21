f96e015eae3793c5a8a67a0c6b574f6cf847d04d
BagusDuwiPrasetiyo <bagusduwi9@gmail.com>
2019-10-21 15:31:41 +0700

    menambah config.php

---
+++ project-repairme/app/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('BASEURL', 'http://localhost/php/phpmvc/public');
+
+
+//Database
+define('db_HOST', 'localhost');
+define('db_USER', 'root');
+define('db_PASS', '');
+define('db_NAME', 'phpmvc');
+
