ffd7d9395b234d50a6566006fcb94da3c959baad
Febiola Putri Yunita <febiolaputri01@gmail.com>
2019-10-21 15:37:53 +0700

    nav home

--- project-repairme/app/config/config.php
+++ project-repairme/app/config/config.php
@@ -1,11 +1,11 @@
 <?php 
 
-define('BASEURL', 'http://localhost/php/phpmvc/public');
+define('BASEURL', 'http://localhost/project-repairme/public');
 
 
 //Database
-define('db_HOST', 'localhost');
-define('db_USER', 'root');
-define('db_PASS', '');
-define('db_NAME', 'phpmvc');
+//define('db_HOST', 'localhost');
+//define('db_USER', 'root');
+//define('db_PASS', '');
+//define('db_NAME', 'phpmvc');
 
--- project-repairme/app/controllers/Home.php
+++ project-repairme/app/controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends Controller{
 	public function index(){
 		//membuat variable data, untuk mengisikan parameter, title(cek di views/home/index.php);
 		$data['judul'] = 'Home';
-		$data['nama'] = $this->model('Mahasiswa_model')->getNama();
+		//$data['nama'] = $this->model('Mahasiswa_model')->getNama();
 		//memangil method view dengan parameter $data
 		//templates/header -> untuk lokasi filenya ada dimana
 		$this->view('templates/header', $data);
