6b20d54a41c7043799b7a300461aecf83d4453f9
agung <joyoagung170@gmail.com>
2019-11-03 19:24:23 -0800

    coba

---
+++ individu/agung/register/regispelanggan.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrasi Pelanggan</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+</head>
+<body bgcolor="#999;">
+	<div class="box-form">
+
+		<div class="title-form">
+			Registrasi Pelanggan
+	</div>
+
+	<h1></h1>
+	<br>
+		<center>
+			Untuk pendaftaran sebagai Mitra, klik
+			<a href="regismitra.php"> disini </a>
+		</center>
+	</br>
+
+	<div class="content-form">
+		<form action="" method="post">
+			No.KTP<br>
+			<input type="text" /><br>
+
+			No.HP<br>
+			<input type="text" /><br>
+
+			Username<br>
+			<input type="text" /><br>
+
+			Password<br>
+			<input type="text" /><br>
+
+			Ulangi Passsword<br>
+			<input type="text" /><br>
+
+			Email
+			<textarea></textarea><br>
+			<input type="submit" />
+		</form>
+	</div>
+
+</div>
+</body>
+</html>
