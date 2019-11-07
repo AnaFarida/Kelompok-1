6ef7df333d8d73866a9805799c0b3d13b556a531
AnaFarida <55472044+AnaFarida@users.noreply.github.com>
2019-11-07 13:14:47 +0700

    update

--- project-repairme/app/views/templates/header.php
+++ project-repairme/app/views/templates/header.php
@@ -20,7 +20,7 @@
    
      <div class="navbar-nav">
       <a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="<?= BASEURL; ?>/mitra">Mitra</a>
+      <a class="nav-item nav-link" href="<?= BASEURL; ?>/mitra">Mitra</a><a class="nav-item nav-link" href="<?= BASEURL; ?>/login">Login</a>
       
 	    </div>
 	  </div>
