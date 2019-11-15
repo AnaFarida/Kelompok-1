4638cb6f7751955392f3006059d8847d7b949176
AnaFarida <55472044+AnaFarida@users.noreply.github.com>
2019-11-15 19:31:17 +0700

    homeSources belum selesai

--- project-repairme/app/views/templates/footerHome.php
+++ project-repairme/app/views/templates/footerHome.php
@@ -1,11 +1,9 @@
-<script src="<?= BASEURL; ?>/Homesource/assets/libs/js/jquery-3.3.1.min.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/bootstrap.min.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/jquery.slicknav.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/owl.carousel.min.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/jquery.magnific-popup.min.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/circle-progress.min.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/jquery.barfiller.js"></script>
-    <script src="<?= BASEURL; ?>/Homesource/assets/libs/main.js"></script>
-    </body>
- 
-</html>
+<script type="text/javascript" src="homeSources/assets/libs/js/jquery.1.8.3.min.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/bootstrap.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/jquery-scrolltofixed.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/jquery.isotope.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/wow.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/classie.js"></script>
+	<script type="text/javascript" src="homeSources/assets/libs/js/magnific-popup.js"></script>
+	<script src="homeSources/assets/vendor/contactform/contactform.js"></script>
