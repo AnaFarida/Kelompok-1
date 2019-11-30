1dcf4423b2469594f01efd458287b8b3ddd30324
Febiola Putri Yunita <febiolaputri01@gmail.com>
2019-11-30 13:51:20 +0700

    insertkategori.admin

--- project-repairme/app/views/admin/tambahKategori.php
+++ project-repairme/app/views/admin/tambahKategori.php
@@ -7,19 +7,7 @@
         <!-- ============================================================== -->
         <div class="row">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                <div class="page-header">
-                    <h2 class="pageheader-title">Input barang</h2>
-                    <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
-                    <div class="page-breadcrumb">
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
-                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Input</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Tambah barang</li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
+               
             </div>
         </div>
         
@@ -30,7 +18,8 @@
             <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
                 <div class="card">
                     <div class="card-body">
-                        <form action="" method="POST" id="formKat" data-parsley-validate="">
+                    <P >INPUT DATA</P>
+                        <form action="<?= BASEURL;?>/admin/insertbarang" method="POST" id="formKat" data-parsley-validate="">
                             <div class="form-group">
                                 <table>
                                     <tr>
@@ -39,7 +28,7 @@
                                     <tr>
                                         <td>
                                             
-                                            <select name="kategori" id="kategori">
+                                            <select class="form-control" name="kategori" id="kategori">
                                                 <option value="">pilih kategori</option>
                                                 <?php foreach ($data['kategori']['kategori'] as $kategori):?>
                                                 <option value=""><?= $kategori['kategori']; ?></option>
@@ -49,14 +38,14 @@
                                         <td>
                                             
                                             <div class="col-sm-6 pl-0">
-                                                <p class="text-right">
-                                                    <button ><a href="" data-toggle="modal" data-target="#formmodkat">+</a></button>
-                                                    <!--   <button class="btn btn-space btn-secondary">Cancel</button> -->
-                                                </td>
-                                            </tr>
-                                        </table>
+                                               
+                                                <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#formmodkat">+
+                                                                    </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
                                     </div>
-                                    <form action="" method="POST" id="formmodmerk" data-parsley-validate="">
                                         <div class="form-group">
                                             <table>
                                                 <tr>
@@ -64,7 +53,7 @@
                                                 </tr>
                                                 <tr>
                                                     <td>
-                                                     <select name="merk" id="merk">
+                                                     <select class="form-control" name="merk" id="merk">
                                                         <option value="">pilih merk</option>
                                                     <?php foreach ($data['kategori']['merk'] as $merk):?>
                                                     <option value=""><?= $merk['merk']; ?></option>
@@ -73,19 +62,15 @@
                                                     <td>
                                                         
                                                         <div class="col-sm-6 pl-0">
-                                                            <p class="text-right">
-                                                                <button >
-                                                                    <a href="" data-toggle="modal" data-target="    #formmerk">+
-                                                                    </a>
-                                                                </button>
-                                                            </p>
+                                        
+                                                            <button type="button" class="btn btn-dark" data-toggle="modal" data-target=" #formmerk">+
+                                                                    </button>
                                                         </div>        
                                                     </td>
                                                 </tr>
                                             </table>
                                         </div>
-                                    </form>
-                                   <form action="" method="POST" id="formmodtipe" data-parsley-validate="">
+                                   
                                         <div class="form-group">
                                             <table>
                                                 <tr>
@@ -93,7 +78,7 @@
                                                 </tr>
                                                 <tr>
                                                     <td>
-                                                      <select name="tipe" id="tipe">
+                                                      <select class="form-control" name="tipe" id="tipe">
                                                         <option value="">pilih tipe</option>
                                                         <?php foreach ($data['kategori']['tipe'] as $tipe):?>
                                                         <option value=""><?= $tipe['tipe']; ?></option>
@@ -103,15 +88,21 @@
                                                     <td>
                                                         
                                                         <div class="col-sm-6 pl-0">
-                                                            <p class="text-right">
-                                                                <button >
-                                                                    <a href="" data-toggle="modal" data-target="    #formtipe">+
-                                                                    </a>
-                                                                </button>
-                                                            </p>
+                                                          
+                                                            <button type="button" class="btn btn-dark" data-toggle="modal" data-target=" #formtipe">+
+                                                                    </button>
+                                                               
+                                    
                                                         </div>        
                                                     </td>
                                                 </tr>
+                                            <tr>
+                                            <td>
+                                            <br><br>
+                                            <button type="submit" class="btn btn-dark" name="submit3" id="submit3">kirim
+                                                                    </button>
+                                            </td>
+                                            </tr>
                                             </table>
                                         </div>
                                     </form>                 
@@ -119,31 +110,8 @@
                 </div>
             </div>
         </div>
-                                        <!-- ============================================================== -->
-                                        <!--end world map -->
-                                        <!-- ============================================================== -->
-                                        <!-- ============================================================== -->
-                                        <!-- footer -->
-                                        <!-- ============================================================== -->
-                                        <div class="footer">
-                                            <div class="container-fluid">
-                                                <div class="row">
-                                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                                                        Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
-                                                    </div>
-                                                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                                                        <div class="text-md-right footer-links d-none d-sm-block">
-                                                            <a href="javascript: void(0);">About</a>
-                                                            <a href="javascript: void(0);">Support</a>
-                                                            <a href="javascript: void(0);">Contact Us</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!-- ============================================================== -->
-                                        <!-- end footer -->
-                                        <!-- ============================================================== -->
+                         
+                                   
                                     </div>
                                 </div>
                                 <!-- ============================================================== -->
@@ -169,7 +137,7 @@
                                                     <label for="kategori">kategori</label>
                                                     <input id="kategori" type="text" name="kategori" data-parsley-trigger="change"  placeholder="isikan kategori" autocomplete="off" class="form-control">
                                                     
-                                                    <button type="submit" name="submit" id="submit">kirim</button>
+                                                    <button type="submit" name="submit" id="submit" class="btn btn-secondary">kirim</button>
                                                     
                                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                                 </form>
@@ -198,7 +166,7 @@
                                                 <label for="merk">merk</label>
                                                 <input id="merk" type="text" name="merk" data-parsley-trigger="change"  placeholder="isikan merk" autocomplete="off" class="form-control">
                                                 
-                                                <button type="submit" name="submit1" id="submit1">kirim</button>
+                                                <button type="submit" name="submit1" id="submit1" class="btn btn-secondary">kirim</button>
                                                 
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                             </form>
@@ -228,7 +196,7 @@
                                                 <label for="tipe">Tipe</label>
                                                 <input id="tipe" type="text" name="tipe" data-parsley-trigger="change"  placeholder="isikan tipe" autocomplete="off" class="form-control">
                                                 
-                                                <button type="submit" name="submit2" id="submit2">kirim</button>
+                                                <button type="submit" name="submit2" id="submit2" class="btn btn-secondary">kirim</button>
                                                 
                                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                             </form>
@@ -238,3 +206,18 @@
                             </div>
                         </div>
 
+                        <div class="listMitra">
+		<h3>Daftar BARANG</h3>
+			<?php foreach($data['mitra'] as $mitra) :?>
+			<ul class="list-group">
+				<li class="list-group-item" >
+					<?= $mitra['nama_usaha']; ?>
+					<!-- <a href="<?= BASEURL; ?>/mitra/delete/<?= $mitra['id_mitra']; ?>" class="badge badge-danger float-right ml-1">Hapus</a>
+					<a href="<?= BASEURL; ?>/mitra/ubah/<?= $mitra['id_mitra']; ?>/<?= $mitra['id_user']; ?>" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mitra['id']; ?>">Ubah</a>
+					<a href="<?= BASEURL; ?>/mitra/detailMitra/<?= $mitra['id_mitra']; ?>" class="badge badge-primary float-right ml-1">Detail</a> -->
+					
+				</li>
+			</ul>
+			<?php endforeach; ?>
+			
+	</div>
