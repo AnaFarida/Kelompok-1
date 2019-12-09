0cdd2957cd44ac3eb51a3bd13639140c720f6c79
AnaFarida <55472044+AnaFarida@users.noreply.github.com>
2019-12-10 02:55:35 +0700

    daftar kerusakan hp dan Laptop dan Portfolio Mitra

--- project-repairme/app/views/admin/kerusakan/tambahkerusakanlaptop.php
+++ project-repairme/app/views/admin/kerusakan/tambahkerusakanlaptop.php
@@ -49,6 +49,29 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
+           
+           <div class="card-body">
+
+              <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="custom-select custom-select-sm form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-12 col-md-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
+                
+                <thead>
+                <tr role="row"><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 10px;">No</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 194px;">Kerusakan</th></tr>
+                </thead>
+
+                <tbody>
+                   <?php foreach ($data['kerusakan'] as $kerusakan) :?>
+                <tr>
+                    <td><?= $kerusakan['id_kerusakan_laptop']; ?></td>
+                    <td><?= $kerusakan['kerusakan_laptop']; ?></td>                   
+                </tr>
+                 <?php endforeach; ?>
+                  </tbody>
+               
+              </table>
+               
+            </div>
+            
+            </div>
           </div>
         </div>
       </div>
